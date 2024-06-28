1e1a2667eeb7cee1bde9d3b8f9fd17b4e7af3f61
nk44 <joshua.fabian1@gmail.com>
2024-06-28 14:41:38 +0200

    Update LBP.sh

--- LBP.sh
+++ LBP.sh
@@ -7,10 +7,10 @@ DEFAULT='\e[0m'
 # Überprüfen, ob tlp installiert ist
 if ! command -v tlp &> /dev/null
 then
-    echo "tlp ist nicht installiert. Installation wird gestartet..."
+    echo "tlp is not installed. Installation is started..."
     sudo apt-get install tlp -y
 else
-    echo "tlp ist bereits installiert."
+    echo "tlp is already installed."
 fi
 clear
 sudo systemctl restart tlp
@@ -20,35 +20,35 @@ if [ -f "$CONFIG" ]; then
     VALUE1=$(grep '^START_CHARGE_THRESH_BAT0=' "$CONFIG" | cut -d'=' -f2)
     VALUE2=$(grep '^STOP_CHARGE_THRESH_BAT0=' "$CONFIG" | cut -d'=' -f2)
     if [ "$VALUE1" -eq 40 ] && [ "$VALUE2" -eq 80 ]; then
-    echo -e "der Akkuschoner ist ${GREEN}aktiv${DEFAULT}"
+    echo -e "the battery saver is ${GREEN}active${DEFAULT}"
     elif [ "$VALUE1" -eq 100 ] && [ "$VALUE2" -eq 100 ]; then
-    echo -e "der Akkuschoner ist ${RED}nicht aktiv${DEFAULT}"    
+    echo -e "the battery saver is ${RED}not active${DEFAULT}"    
     else
-    echo "Der Wert von START_CHARGE_THRESH_BAT0 ist $VALUE1."
-    echo "Der Wert von STOP_CHARGE_THRESH_BAT0 ist $VALUE2."    
+    echo "The value of START_CHARGE_THRESH_BAT0 is $VALUE1."
+    echo "The value of STOP_CHARGE_THRESH_BAT0 is $VALUE2."    
     fi
 else
-    echo "Die Datei $CONFIG existiert nicht."
+    echo "The $CONFIG file does not exist."
 fi
 
-read -p "Akkuschoner aktivieren (1), deaktivieren (2) oder abbrechen (3): " choice
+read -p "Activate (1), deactivate (2) or cancel (3) the battery saver: " choice
 if [ "$choice" == "1" ]; then
     sudo sed -i 's/^START_CHARGE_THRESH_BAT0=[0-9]*$/START_CHARGE_THRESH_BAT0=40/' "$CONFIG"
     sudo sed -i 's/^STOP_CHARGE_THRESH_BAT0=[0-9]*$/STOP_CHARGE_THRESH_BAT0=80/' "$CONFIG"
-    echo -e "der Akkuschoner wurde ${GREEN}aktiviert${DEFAULT}"
+    echo -e "the battery saver has been ${GREEN}activated${DEFAULT}"
     sudo systemctl restart tlp
-    read -n 1 -s -r -p "Drücken Sie eine beliebige Taste, um das Skript zu beenden."
+    read -n 1 -s -r -p "Press any key to exit the script."
 elif [ "$choice" == "2" ]; then
     sudo sed -i 's/^START_CHARGE_THRESH_BAT0=[0-9]*$/START_CHARGE_THRESH_BAT0=100/' "$CONFIG"
     sudo sed -i 's/^STOP_CHARGE_THRESH_BAT0=[0-9]*$/STOP_CHARGE_THRESH_BAT0=100/' "$CONFIG"
-    echo -e "der Akkuschoner wurde ${RED}deaktiviert${DEFAULT}"     
+    echo -e "the battery saver has been ${RED}deactivated${DEFAULT}"     
     sudo systemctl restart tlp
-    read -n 1 -s -r -p "Drücken Sie eine beliebige Taste, um das Skript zu beenden."
+    read -n 1 -s -r -p "Press any key to exit the script."
 elif [ "$choice" == "3" ]; then
-    read -n 1 -s -r -p "Drücken Sie eine beliebige Taste, um das Skript zu beenden."
+    read -n 1 -s -r -p "Press any key to exit the script."
    exit
 else
-    read -n 1 -s -r -p "Ungültige Auswahl! Script wird beendet."
+    read -n 1 -s -r -p "Invalid selection! Script is terminated."
 fi
 
 
